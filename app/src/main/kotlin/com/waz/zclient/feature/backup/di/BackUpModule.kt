f85ae862f89d01e2b1bb5ea4958419e29724c2d8
Maciej Gorywoda <makingthematrix@gmail.com>
2020-08-18 17:42:50 +0200

    New backups: Remove encryption
    This PR removes EncryptionHandler and associated code. I merged EncryptionHandler before by mistake. It turned out it's easier to "revert" it
    manually than to do `git revert` and fix conflicts. Lesson learned. I'll be more careful in the future.

--- app/src/main/kotlin/com/waz/zclient/feature/backup/di/BackUpModule.kt
+++ app/src/main/kotlin/com/waz/zclient/feature/backup/di/BackUpModule.kt
@@ -4,7 +4,6 @@ import com.waz.zclient.core.utilities.converters.JsonConverter
 import com.waz.zclient.feature.backup.BackUpRepository
 import com.waz.zclient.feature.backup.BackUpViewModel
 import com.waz.zclient.feature.backup.zip.ZipHandler
-import com.waz.zclient.feature.backup.encryption.EncryptionHandler
 import com.waz.zclient.feature.backup.assets.AssetsBackUpModel
 import com.waz.zclient.feature.backup.assets.AssetsBackupDataSource
 import com.waz.zclient.feature.backup.assets.AssetsBackupMapper
@@ -23,7 +22,6 @@ import com.waz.zclient.feature.backup.conversations.ConversationsBackupMapper
 import com.waz.zclient.feature.backup.conversations.ConversationMembersBackUpModel
 import com.waz.zclient.feature.backup.conversations.ConversationMembersBackupDataSource
 import com.waz.zclient.feature.backup.conversations.ConversationMembersBackupMapper
-import com.waz.zclient.feature.backup.encryption.EncryptionHandlerDataSource
 import com.waz.zclient.feature.backup.folders.FoldersBackUpModel
 import com.waz.zclient.feature.backup.folders.FoldersBackupDataSource
 import com.waz.zclient.feature.backup.folders.FoldersBackupMapper
@@ -88,9 +86,8 @@ val backupModules: List<Module>
 val backUpModule = module {
     single { androidContext().externalCacheDir }
     single { ZipHandlerDataSource(get()) } bind ZipHandler::class
-    single { EncryptionHandlerDataSource() } bind EncryptionHandler::class
 
-    factory { CreateBackUpUseCase(getAll(), get(), get(), get()) } //this resolves all instances of type BackUpRepository
+    factory { CreateBackUpUseCase(getAll(), get(), get()) } //this resolves all instances of type BackUpRepository
     viewModel { BackUpViewModel(get()) }
 
     // MetaData
