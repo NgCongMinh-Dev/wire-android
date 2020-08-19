33b9807244877b35dabaecc77665de3b55efd1d9
Maciej Gorywoda <makingthematrix@gmail.com>
2020-08-19 10:50:36 +0200

    Rewriting ZipHandlerTest

--- app/src/test/kotlin/com/waz/zclient/feature/backup/ZipHandlerTest.kt
+++ app/src/test/kotlin/com/waz/zclient/feature/backup/ZipHandlerTest.kt
@@ -2,6 +2,9 @@ package com.waz.zclient.feature.backup
 
 import com.waz.zclient.UnitTest
 import com.waz.zclient.core.functional.Either
+import com.waz.zclient.core.functional.onFailure
+import com.waz.zclient.core.functional.onSuccess
+import com.waz.zclient.feature.backup.zip.ZipFailure
 import com.waz.zclient.feature.backup.zip.ZipHandler
 import com.waz.zclient.feature.backup.zip.ZipHandlerDataSource
 import com.waz.zclient.feature.createTextFile
@@ -22,28 +25,20 @@ class ZipHandlerTest : UnitTest() {
 
         val zipHandler: ZipHandler = ZipHandlerDataSource(tempDir)
 
-        when (val res = zipHandler.zip(uniqueZipFileName(), listOf(textFile))) {
-            is Either.Left -> fail(res.a.toString())
-            is Either.Right -> {
-                val zipFile = res.b
-                zipFile.length() `should be greater than` 0
-            }
-        }
+        zipHandler.zip(uniqueZipFileName(), listOf(textFile))
+            .onSuccess { it.length() `should be greater than` 0 }
+            .onFailure { fail(it.toString()) }
     }
 
     @Test
-    fun `given no input files, when zipped, then return a zipped file`() {
+    fun `given no input files, when zipped, then return a zip failure`() {
         val tempDir = createTempDir()
 
         val zipHandler: ZipHandler = ZipHandlerDataSource(tempDir)
 
-        when (val res = zipHandler.zip(uniqueZipFileName(), emptyList())) {
-            is Either.Left -> fail(res.a.toString())
-            is Either.Right -> {
-                val zipFile = res.b
-                zipFile.length() `should be greater than` 0
-            }
-        }
+        zipHandler.zip(uniqueZipFileName(), emptyList())
+            .onSuccess { fail("The test should fail with ZipFailure") }
+            .onFailure { assert(it is ZipFailure) }
     }
 
     @Test
@@ -53,13 +48,9 @@ class ZipHandlerTest : UnitTest() {
 
         val zipHandler: ZipHandler = ZipHandlerDataSource(tempDir)
 
-        when (val res = zipHandler.zip(uniqueZipFileName(), fileList)) {
-            is Either.Left -> fail(res.a.toString())
-            is Either.Right -> {
-                val zipFile = res.b
-                zipFile.length() `should be greater than` 0
-            }
-        }
+        zipHandler.zip(uniqueZipFileName(), fileList)
+            .onSuccess { it.length() `should be greater than` 0 }
+            .onFailure { fail(it.toString()) }
     }
 
     @Test
@@ -74,15 +65,13 @@ class ZipHandlerTest : UnitTest() {
         assert(zipped.isRight)
         val zipFile = (zipped as Either.Right<File>).b
 
-        when (val res = zipHandler.unzip(zipFile)) {
-            is Either.Left -> fail(res.a.toString())
-            is Either.Right -> {
-                val unzipped = res.b
-                unzipped.size shouldEqual 1
-                val unzippedContents = unzipped.first().readText()
+        zipHandler.unzip(zipFile)
+            .onSuccess {
+                it.size shouldEqual 1
+                val unzippedContents = it.first().readText()
                 unzippedContents shouldEqual originalContents
             }
-        }
+            .onFailure { fail(it.toString()) }
     }
 
     @Test
@@ -103,16 +92,14 @@ class ZipHandlerTest : UnitTest() {
         assert(zipped.isRight)
         val zipFile = (zipped as Either.Right<File>).b
 
-        when (val res = zipHandler.unzip(zipFile)) {
-            is Either.Left -> fail(res.a.toString())
-            is Either.Right -> {
-                val unzipped = res.b
-                unzipped.size shouldEqual 2
-                val unzippedContents1 = unzipped.first().readText()
-                unzippedContents1 shouldEqual originalContents[unzipped.first().name]
-                val unzippedContents2 = unzipped[1].readText()
-                unzippedContents2 shouldEqual originalContents[unzipped[1].name]
+        zipHandler.unzip(zipFile)
+            .onSuccess {
+                it.size shouldEqual 2
+                val unzippedContents1 = it.first().readText()
+                unzippedContents1 shouldEqual originalContents[it.first().name]
+                val unzippedContents2 = it[1].readText()
+                unzippedContents2 shouldEqual originalContents[it[1].name]
             }
-        }
+            .onFailure { fail(it.toString()) }
     }
 }
